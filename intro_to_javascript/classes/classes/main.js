2d4d2a3d74f065b881dd0066a43382f4d17da067
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-05-23 18:19:40 +0100

    Inheritance and Static methods

--- intro_to_javascript/classes/classes/main.js
+++ intro_to_javascript/classes/classes/main.js
@@ -36,4 +36,51 @@ surgeonCurry.takeVacationDays(3);
 console.log(surgeonCurry.remainingVacationDays)
 
 
+//Inheritance
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+
+  //Static method
+  static generatePassword(){
+    return Math.floor(Math.random() * 10000);
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+
 
