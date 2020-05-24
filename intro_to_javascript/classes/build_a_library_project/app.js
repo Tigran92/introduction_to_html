2f684b2e6f418606c299b9c5cd79603e25c9b280
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-05-24 19:53:54 +0100

    Build A Library-2

--- intro_to_javascript/classes/build_a_library_project/app.js
+++ intro_to_javascript/classes/build_a_library_project/app.js
@@ -42,20 +42,64 @@ class Media {
     return ratingSum/lengthOfArray;
 
     
-
-    
   }
 
   addRating(addRate) {
-
     this._ratings.push(addRate);
+  }
+
+};
+
+class Book extends Media {
+  constructor(author, title, pages){
+    super(title);
+    this._author = author;
+    this._pages = pages;
 
   }
 
-  
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+};
+
+class Movie extends Media {
+  constructor(director, title, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+};
+
+const historyOfEverything = new Book('Bill Bryson', 'A short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
 
 
-}
 
-const media = new Media('dddd');
-console.log(media.toggleCheckOutStatus)
+console.log(historyOfEverything.getAverageRating());
+
+
+
+
+
+
