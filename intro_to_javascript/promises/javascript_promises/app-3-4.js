6a6fee9cebab498ba29866ec0959202cba3dcb0c
Tigran92 <22913312+Tigran92@users.noreply.github.com>
2020-06-07 23:57:18 +0100

    JavaScript Promises

---
+++ intro_to_javascript/promises/javascript_promises/app-3-4.js
@@ -0,0 +1,54 @@
+//Constructing a Promise Object
+const inventory = {
+  sunglasses: 1900,
+  pants: 1088,
+  bags: 1344
+};
+
+// Write your code below:
+const myExecutor = (resolve, reject) => {
+
+  if(inventory.sunglasses > 0 ) {
+    resolve('Sunglasses order processed.');
+  } else {
+    reject('That item is sold out.');
+  }
+
+};
+
+const orderSunglasses = () => {
+
+  return new Promise(myExecutor)
+};
+
+const orderPromise = orderSunglasses();
+
+console.log(orderPromise);
+
+
+//The Node setTimeout() Function
+console.log("This is the first line of code in app.js.");
+
+// Keep the line above as the first line of code
+// Write your code here:
+
+const usingSTO = () => {
+  console.log('Testing')
+};
+
+setTimeout(usingSTO, 1500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
---
+++ intro_to_javascript/promises/javascript_promises/app-6.js
@@ -0,0 +1,19 @@
+//The onFulfilled and onRejected Functions
+const {checkInventory} = require('./library-6.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+const handleSuccess = (resolvedValue) => {
+    console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+    console.log(rejectReason);
+};
+
+checkInventory(order)
+    .then(handleSuccess, handleFailure);
+
+
+
